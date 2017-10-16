f261a2e3e380d4b81ec2360aa63d11d4aa8ce537
Jim <jim.burns14@gmail.com>
2017-10-16 13:17:29 -0400

    contact form update

--- routes/main.js
+++ routes/main.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var router  = express.Router();
+var express    = require('express');
+var router     = express.Router();
+var bodyParser = require('body-parser');
 
 // controllers
 var main_controller = require('../controllers/mainController');
